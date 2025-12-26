cf8c8f5c12c29da9e422847248b3671bd8ee34a6
johannesroesner <roesnerj@icloud.com>
2025-12-26 10:39:33 +0100

    feat: init dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="card bg-base-100 w-96 shadow-sm">
+	<figure>
+		<img
+			src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+			alt="Shoes" />
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">Card Title</h2>
+		<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary">Buy Now</button>
+		</div>
+	</div>
+</div>
