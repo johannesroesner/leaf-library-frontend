6cb0d51373fd1df4e75fadd5782fadec2a6a80ad
johannesroesner <roesnerj@icloud.com>
2025-12-28 17:37:27 +0100

    feat: crud for plants finished

---
+++ src/lib/ui/Toolbar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  let {updateStatus = $bindable(false), deleteFunction} = $props();
+
+  function goBack() {
+    history.back();
+  }
+  function setUpdateStatus() {
+    updateStatus = !updateStatus;
+  }
+</script>
+
+<div class="flex justify-between items-center mb-5">
+  <button class="btn btn-neutral" onclick={goBack}>
+    Back
+  </button>
+  <div class="space-x-2">
+    <button class="btn btn-neutral" onclick={setUpdateStatus}>
+      Update
+    </button>
+    <button class="btn btn-error" onclick={deleteFunction}>
+      Delete
+    </button>
+  </div>
+</div>
