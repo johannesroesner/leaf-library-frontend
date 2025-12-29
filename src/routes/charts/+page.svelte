b7f3a7b43447c7863f9fa40a44762df87694d857
Johannes Rösner <roesnerj@icloud.com>
2025-12-29 23:07:05 +0100

    feat: PlantBiomeBarChart.svelte and PlantTypePieChart.svelte (#7)

---
+++ src/routes/charts/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import PlantTypePieChart from "$lib/ui/PlantTypePieChart.svelte";
+  import PlantBiomeBarChart from "$lib/ui/PlantBiomeBarChart.svelte";
+</script>
+
+<div class="flex">
+  <PlantTypePieChart />
+  <PlantBiomeBarChart />
+</div>
